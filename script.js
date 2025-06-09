71b626edf1f72b172f5afd642cf1297609fe23c7
ayomipo <ayomiposoyinka@gmail.com>
2025-06-09 12:28:15 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function slideLeft() {
+  const container = document.getElementById("cardContainer");
+  container.scrollLeft -= 300;
+}
+
+function slideRight() {
+  const container = document.getElementById("cardContainer");
+  container.scrollLeft += 300;
+}
